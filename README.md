1830b40b909866eee369b2f31a5b7c20958c2f30
RomChung <thelns@vip.qq.com>
2021-02-23 18:06:14 +0800

    add: Readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Wasm Template
+
+**Template for Wasm, Snowpack**
--- build/web/index.js
+++
@@ -1,7 +0,0 @@
-import init, { add } from '../dist/wasm-demo/index.js';
-
-const start = async () => {
-  await init();
-  console.log(add(1, 2));
-}
-start()
